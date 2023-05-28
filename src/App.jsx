6125b0008e2f86f05ffcb06c5c51e8374933c859
sebasr20 <sebasr20@gmail.com>
2023-05-28 19:20:25 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import CharacterList from './components/CharacterList';
+import Character from './components/Character';
+
+function App() {
+
+  return <div className='bg-dark text-white'>
+    <h1 className='text-center display-1 py-4'>Rick and Morty</h1>
+    <CharacterList/>
+    
+  </div>
+}
+
+export default App
